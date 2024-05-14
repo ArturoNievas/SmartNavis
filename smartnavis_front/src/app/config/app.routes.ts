442a48b2b0429bb638fcc41cc56a5b7e50893938
juanGalati <jcgalati14@gmail.com>
2024-05-14 17:42:37 -0300

    Estilos Embarcaciones Publicadas

--- smartnavis_front/src/app/config/app.routes.ts
+++ smartnavis_front/src/app/config/app.routes.ts
@@ -1,14 +1,27 @@
-import {Routes} from '@angular/router';
-import {HomePageComponent} from "../pages/home-page/home-page.component";
-import {PuertosPageComponent} from "../pages/puertos-page/puertos-page.component";
+import { Routes } from '@angular/router';
+import { HomePageComponent } from '../pages/home-page/home-page.component';
+import { PuertosPageComponent } from '../pages/puertos-page/puertos-page.component';
 import { EmbarcacionesPageComponent } from '../pages/embarcaciones-page/embarcaciones-page.component';
 import { EmbarcacionesPublicadasPageComponent } from '../pages/embarcaciones-publicadas-page/embarcaciones-publicadas-page.component';
+import { PublicacionEmbarcacionPageComponent } from '../pages/publicacion-embarcacion-page/publicacion-embarcacion-page.component';
 
 export const routes: Routes = [
-  {path: 'home', title: 'Home', component: HomePageComponent},
-  {path: 'puertos', title: 'Puertos', component: PuertosPageComponent},
-  {path: 'embarcaciones', title: 'Embarcaciones', component: EmbarcacionesPageComponent},
-  {path: 'embarcaciones/publicaciones', title: 'Publicaciones', component: EmbarcacionesPublicadasPageComponent},
-  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+  { path: 'home', title: 'Home', component: HomePageComponent },
+  { path: 'puertos', title: 'Puertos', component: PuertosPageComponent },
+  {
+    path: 'embarcaciones',
+    title: 'Embarcaciones',
+    component: EmbarcacionesPageComponent,
+  },
+  {
+    path: 'embarcaciones/publicaciones',
+    title: 'Publicaciones',
+    component: EmbarcacionesPublicadasPageComponent,
+  },
+  {
+    path: 'embarcaciones/publicaciones/:id',
+    title: 'Publicaciones',
+    component: PublicacionEmbarcacionPageComponent,
+  },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
-
