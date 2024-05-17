9dac6dc1ac0dc9510e96b4d93783354567d14fcf
juanGalati <jcgalati14@gmail.com>
2024-05-17 16:57:44 -0300

    Eliminar código no utilizado y actualizar maquetas en embarcaciones-publicadas-page.component.ts

--- smartnavis_front/src/app/pages/embarcaciones-publicadas-page/embarcaciones-publicadas-page.component.ts
+++ smartnavis_front/src/app/pages/embarcaciones-publicadas-page/embarcaciones-publicadas-page.component.ts
@@ -4,15 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppPageComponent } from '../../shared/components/app-page/app-page.component';
 
-import { mockups } from '../../shared/mockups';
-
-const publicaciones = Array.from({ length: 10 }, (_, i) => ({
-  id: i + 1,
-  titulo: `Embarcación ${i + 1}`,
-  descripcion: `Descripción de la embarcación ${i + 1}`,
-  bien: mockups.embarcacion(i + 1),
-  permutacionesSolicitadas: [],
-}));
+const publicaciones: any[] = [];
 
 @Component({
   selector: 'app-embarcaciones-publicadas-page',
