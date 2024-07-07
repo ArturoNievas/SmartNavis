579f79e24836ed0a36f1ab789f01a958c8009602
juanGalati <jcgalati14@gmail.com>
2024-07-07 13:20:26 -0300

    Frontend: Registrar Intercambio - Agregar Endpoint

--- smartnavis_front/src/app/services/permuta/permuta.service.ts
+++ smartnavis_front/src/app/services/permuta/permuta.service.ts
@@ -2,65 +2,6 @@ import { Injectable } from '@angular/core';
 import { ApiService } from '../api/api.service';
 import { Permuta } from '../../interfaces/permuta';
 import { Observable } from 'rxjs';
-import { Bien } from '../../interfaces/bien';
-import { Publicacion } from '../../interfaces/publicacion';
-
-/* public listarPublicaciones(): Observable<Publicacion[]> {
-    return this.apiService.get<Publicacion[]>(this.publicacionesUrl);
-  }
-
-  public crearPublicacion(publicacion: Publicacion): Observable<Publicacion> {
-    return this.apiService.post<Publicacion>(this.publicacionUrl, publicacion);
-  }
-
-  public actualizarPublicacion(
-    publicacion: Publicacion
-  ): Observable<Publicacion> {
-    if (!publicacion.id) {
-      return throwError(
-        () => new Error('No es posible actualizar la publicacion (no id).')
-      );
-    }
-
-    return this.apiService.put<Publicacion>(
-      `${this.publicacionUrl}/${publicacion.id}`,
-      publicacion
-    );
-  }
-
-  public eliminarPublicacion(publicacion: Publicacion): Observable<unknown> {
-    if (!publicacion.id) {
-      return throwError(
-        () => new Error('No es posible eliminar la publicacion (no id).')
-      );
-    }
-    return this.apiService.delete(`${this.publicacionUrl}/${publicacion.id}`);
-  }
-
-  public publicarPublicacion(
-    publicacion: Publicacion
-  ): Observable<Publicacion> {
-    const { __permutasSolicitadas, ..._publicacion } = publicacion;
-
-    console.log(_publicacion);
-
-    return this.apiService.post<Publicacion>(
-      `${this.publicacionUrl}/${publicacion.bien.id}/publicar`,
-      _publicacion
-    );
-  }
-
-  public solicitarIntercambio(
-    publicacionSolicitada: Publicacion,
-    publicacionOfertada: Publicacion
-  ): any {
-    return this.apiService.post(
-      `${this.publicacionUrl}/${publicacionSolicitada.bien.id}/solicitar`,
-      publicacionOfertada.id
-    );
-  } */
-
-/* POST ../permuta/{:id}/aceptar */
 
 @Injectable({
   providedIn: 'root',
@@ -79,7 +20,7 @@ export class PermutaService {
 
   public registrarPermuta(permuta: Permuta): Observable<Permuta> {
     return this.apiService.post<Permuta>(
-      `this.permutaUrl/${permuta.id}/registrar`
+      `${this.permutaUrl}/${permuta.id}/registrar`
     );
   }
 }
